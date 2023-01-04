0bea41f930de2c1b71e9682260f1b04f775a5f82
Keb <45945415+kebgib@users.noreply.github.com>
2023-01-04 11:02:15 -0800

    Update geolocate_ip.py

--- geolocate_ip.py
+++ geolocate_ip.py
@@ -1,29 +1,36 @@
 #! python3
 # -*- coding: utf-8 -*-
-
+"""
+Geolocate an IP address
+Find the documentation here:
+https://tools.keycdn.com/geo
+"""
 import requests
-import json
-from time import sleep
 
-
-def locate():
+def Geolocate(ip_address):
     """
-    This function performs an HTTPS GET of the geolocation API and returns a JSON object
-    containing information of the chosen IP.
-    :return: none - exhibits print of information
+    Perform a GET of the geolocation API and returns a JSON object
     """
-    ip = input("Geolocate IP: ")
-    url = f"https://tools.keycdn.com/geo.json?host={ip}"
-    response = requests.get(url,
-                            timeout=2)
-    response_json = json.loads(response.text)
-    result = (response_json['status'])
-    print(f"Result -- {result}")
-    for field in response_json['data']['geo']:
-		    if str(response_json['data']['geo'][field]) != 'None': print(field.title() + " -- " + str(response_json['data']['geo'][field]))
+    response = requests.get("https://tools.keycdn.com/geo.json",
+                             params= {'host': ip_address},
+                             headers= {'User-Agent': 'keycdn-tools:https://example.com'},
+                             timeout=10)      
+    return response
 
 if __name__ == '__main__':
     while True:
-        locate()
-        print("\n\n")
-        sleep(3)
+        try:
+            ip = str(input("[+] Geo-locate IP: "))  # str() method to force input to be a string
+            print()
+            response = Geolocate(ip.strip())
+            if response.ok:       
+                # Parse response JSON object and print
+                print(f"[!] {'Result':15} {response.json()['status'].capitalize():10}")
+                for field in response.json()['data']['geo']:    
+                    if response.json()['data']['geo'][field]:
+                        # f-string formatted output, example: f"text {value:width}"
+                        print(f"[+] {field.capitalize():15} {str(response.json()['data']['geo'][field]):10}")
+                print()
+        except Exception as e:
+            print("[!] Failed! - {e}")
+
